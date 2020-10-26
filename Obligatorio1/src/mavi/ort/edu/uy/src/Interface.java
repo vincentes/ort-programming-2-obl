3188499966fab04c1bf464173d54532b8d4c0a0e
matiassallee <48439921+matiassallee@users.noreply.github.com>
2020-10-25 21:45:59 -0300

    add getWinner method to Interface class

--- Obligatorio1/src/mavi/ort/edu/uy/src/Interface.java
+++ Obligatorio1/src/mavi/ort/edu/uy/src/Interface.java
@@ -1,8 +1,11 @@
 package mavi.ort.edu.uy.src;
 
 import java.util.ArrayList;
+import mavi.ort.edu.uy.src.models.Color;
+import mavi.ort.edu.uy.src.models.Disc;
 import mavi.ort.edu.uy.src.models.Match;
 import mavi.ort.edu.uy.src.models.Player;
+import mavi.ort.edu.uy.src.models.ResultCode;
 
 /**
  *
@@ -40,5 +43,121 @@ public class Interface {
         }
         return isPlayerPresent;
     }
+     
+     // This method returns the ResultCode from a discs matrix
+       public static ResultCode getWinner(Disc[][] discs) {
+        String finalResult = "";
+        String finalResultAux = "";
+        for (int x = 1; x <= 6; x++) {
+            for (int y = 1; y <= 6; y++) {
+                Disc currentDisc = discs[x][y];
+                String currentDiscAdjacents = getAdjacents(discs, currentDisc, x, y);
+                if (currentDiscAdjacents.length() > 2) {
+                    String results = "";
+                    for (int i = 1; i <= 6; i++) {
+                        for (int j = 1; j <= 6; j++) {
+                            Disc disc = discs[i][j];
+                            results = adjacentsIntersection(currentDiscAdjacents, getAdjacents(discs, disc, i, j));
+                            if (results.length() > currentDiscAdjacents.length()) {
+                                currentDiscAdjacents = results;
+                            }
+                        }
+                    }
+                    if (results.length() >= finalResultAux.length()) {
+                        finalResultAux = results;
+                        finalResult += finalResultAux + "/";
+                    }
+                }
+            }
+        }
+        return defineWinnerFromResults(discs, finalResult);
+
+    }
+
+    /*This method returns the intersection of a given adjacentes position String 
+      for example let's say that the position 11 has the following adjacent positions 11 21 32 (this String is a valid input)
+    */
+    public static String adjacentsIntersection(String adjacentsPositions, String newAdjacentsPositions) {
+        String[] currencies = newAdjacentsPositions.split(" ");
+        String adjacentsIntersection = adjacentsPositions;
+
+        if (adjacentsPositions.contains(currencies[0])) {
+            for (int i = 1; i < currencies.length; i++) {
+                if (!adjacentsPositions.contains(currencies[i])) {
+                    adjacentsIntersection += " " + currencies[i];
+                }
+            }
+        }
+        return adjacentsIntersection;
+    }
+
+    // Method to return an String with the position of the adjancet's discs of a given disc
+    public static String getAdjacents(Disc[][] discs, Disc disctToValidate, int i, int j) {
+        String adjacentsPositions = "";
+        if (disctToValidate != null) {
+            adjacentsPositions = i + "" + j;
+            if (i - 1 != 0 && discs[i - 1][j] != null && disctToValidate.getColor().equals(discs[i - 1][j].getColor())) {
+                adjacentsPositions += " " + (i - 1) + "" + j;
+            }
+            if (i + 1 != 7 && discs[i + 1][j] != null && disctToValidate.getColor().equals(discs[i + 1][j].getColor())) {
+                adjacentsPositions += " " + (i + 1) + "" + j;
+            }
+            if (j - 1 != 0 && discs[i][j - 1] != null && disctToValidate.getColor().equals(discs[i][j - 1].getColor())) {
+                adjacentsPositions += " " + i + "" + (j - 1);
+            }
+            if (j + 1 != 7 && discs[i][j + 1] != null && disctToValidate.getColor().equals(discs[i][j + 1].getColor())) {
+                adjacentsPositions += " " + i + "" + (j + 1);
+            }
+        }
+        return adjacentsPositions;
+    }
+    
+    // Method to return the ResultCode (PLAYER_RED/PLAYER_BLUE/DRAW) from a given discs matrix and a given finalResult String. 
+    public static ResultCode defineWinnerFromResults(Disc[][] discs, String finalResult) {
+        ResultCode result = ResultCode.DRAW;
+        boolean absolutWinner = true;
+        Color winnerColor;
+
+        String[] currencies = finalResult.split("/");
+
+        int column = Integer.parseInt(String.valueOf(currencies[0].charAt(1)));
+        int row = Integer.parseInt(String.valueOf(currencies[0].charAt(0)));
+        Color colorAux = discs[row][column].getColor();
+
+        int currenciesLenght = currencies[0].length();
+
+        for (int x = 0; x < currencies.length; x++) {
+            column = Integer.parseInt(String.valueOf(currencies[x].charAt(1)));
+            row = Integer.parseInt(String.valueOf(currencies[x].charAt(0)));
+            if (colorAux != discs[row][column].getColor()) {
+                absolutWinner = false;
+            }
+
+        }
+
+        if (!absolutWinner) {
+            for (int i = 0; i < currencies.length; i++) {
+                column = Integer.parseInt(String.valueOf(currencies[i].charAt(1)));
+                row = Integer.parseInt(String.valueOf(currencies[i].charAt(0)));
+                if (currenciesLenght < currencies[i].length()) {
+                    currenciesLenght = currencies[i].length();
+                    winnerColor = discs[row][column].getColor();
+                    if (winnerColor.equals(Color.RED)) {
+                        result = ResultCode.PLAYER_RED;
+                    } else if (winnerColor.equals(Color.BLUE)) {
+                        result = ResultCode.PLAYER_BLUE;
+                    }
+                }
+            }
+        } else {
+            if (colorAux.equals(Color.RED)) {
+                result = ResultCode.PLAYER_RED;
+            } else if (colorAux.equals(Color.BLUE)) {
+                result = ResultCode.PLAYER_BLUE;
+            }
+        }
+        return result;
+    }
+
 
 }
